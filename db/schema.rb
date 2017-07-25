a86f7a5779e3715e74306086dd47d1459345a62d
rubyflames <ralsh39@gmail.com>
2017-07-25 12:00:26 +0800

    rails generate migration CreateJoinTableTripLocation Trip Location

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724073645) do
+ActiveRecord::Schema.define(version: 20170725035129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "Locations_Trips", id: false, force: :cascade do |t|
+    t.bigint "Trip_id", null: false
+    t.bigint "Location_id", null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
