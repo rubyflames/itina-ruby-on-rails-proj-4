1451c92251575eb839adbab86e01d33d10dea3a6
rubyflames <ralsh39@gmail.com>
2017-07-29 12:08:36 +0800

    seeded places and categories, address attribute added to Place model

--- db/migrate/20170725165240_create_places.rb
+++ db/migrate/20170725165240_create_places.rb
@@ -2,6 +2,7 @@ class CreatePlaces < ActiveRecord::Migration[5.1]
   def change
     create_table :places do |t|
       t.string :name
+      t.string :address
       t.decimal :lat, :precision=>10, :scale=>6
       t.decimal :lng, :precision=>10, :scale=>6
       t.float :price_pax
--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170725165506) do
 
   create_table "places", force: :cascade do |t|
     t.string "name"
+    t.string "address"
     t.decimal "lat", precision: 10, scale: 6
     t.decimal "lng", precision: 10, scale: 6
     t.float "price_pax"
